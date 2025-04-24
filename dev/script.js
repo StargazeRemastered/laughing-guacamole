e6a978c8a0d160ac70fc5c6ecaf9bdd21e289084
loveislaced <diddyhasabigdiddler@gmail.com>
2025-04-24 10:49:00 -0800

    Update script.js

--- dev/script.js
+++ dev/script.js
@@ -1,10 +1,3 @@
-// Toggle Light/Dark Theme
-function toggleTheme() {
-  document.body.classList.toggle('dark-theme');
-  const notepad = document.querySelector('.notepad');
-  notepad.classList.toggle('dark-theme');
-}
-
 // Update the title when the user finishes editing
 function updateTitle() {
   const title = document.querySelector('.app-name');
